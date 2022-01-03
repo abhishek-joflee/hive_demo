acb9e5fce2a76bb43bd7c997babf44cfaff122cb
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-03 21:59:06 +0530

    disposed

--- lib/ui/transaction_page.dart
+++ lib/ui/transaction_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({Key? key}) : super(key: key);
@@ -8,6 +9,12 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+  @override
+  void dispose() {
+    Hive.box('transactions').close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
